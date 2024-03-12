5f1b7ec6d0fc162a15e6659817ad122b91ae65a4
Shu Yang <shu.yang@inform-software.com>
2024-03-12 21:30:35 +0100

    route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: 'list', component: RecipeListComponent},];
